abf1542e578923cd185bfc836d260cbbd823904a
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:44:48 +0200

    Adding code examples/obi_curl.sh

---
+++ code examples/obi_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.obi.pl/kabiny-brodziki-i-akcesoria/kabiny-prysznicowe/c/1226"}'
